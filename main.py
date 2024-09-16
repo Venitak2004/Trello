a6b11dc3221d0d5ca21c3a7ac59e0683a172ff67
Venita Kissell <venitak@hotmail.com>
2024-09-16 23:51:17 +1000

    Created User Model and Schema

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from flask import Flask
 from init import db, ma, bcrypt, jwt
 
 #create the app by creating the file and access them
-def create_app();
+def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
 
     db.init_app(app)
     ma.init_app(app)
